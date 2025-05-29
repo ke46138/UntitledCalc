1af6d45f99ba53c9cf5cc3419734c8f94fdeb766
ke46138 <ke46138+gpg@gmail.com>
2025-05-29 21:38:03 +0300

    Добавил ещё функционала для кнопок. Сделал нормальные надписи при делении на 0 и при вычислении корня из отрицательного числа. Теперь дробная часть .0 больше не показывается. Добавил возможность переключения дисплея в read only

--- src/main/java/ru/ke46138/untitledcalc/MainController.java
+++ src/main/java/ru/ke46138/untitledcalc/MainController.java
@@ -7,16 +7,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.util.Duration;
 
+import java.text.DecimalFormat;
+
 public class MainController {
 
     private String charactersOnDisplay = "0";
     private final int maxSymbols = 10;
-    private int currentAction = 0; // 0 - нет действия, 1 - сложение, 2 - вычитание, 3 - деление, 4 - умножение
+    private int currentAction = 0; // 0 - нет действия, 1 - сложение, 2 - вычитание, 3 - деление, 4 - умножение, 5 - вычислить процент, 6 - возвести в степень y
     private double firstNumber = 0;
     private double secondNumber = 0;
     private boolean haveDot = false;
     private final int fadeInDuration = 100;
     private final int fadeOutDuration = 50;
+    private final DecimalFormat df = new DecimalFormat("#.#########");
+    private boolean readOnly = false;
 
     @FXML
     private Label mainDisplay;
@@ -44,83 +48,122 @@ public class MainController {
     @FXML
     private void buttonHandler(ActionEvent event) {
         Button source = (Button) event.getSource();
-        System.out.println("Нажата кнопка: " + source.getText());
-        System.out.println("Длина: " + charactersOnDisplay.length());
         if (source.getId() != null) {
             switch (source.getId()) {
                 case "oneBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "1";
-                    else charactersOnDisplay += "1";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "1";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "1";
+                        else charactersOnDisplay += "1";
+                    }
                     break;
                 case "twoBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "2";
-                    else charactersOnDisplay += "2";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "2";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-2";
+                        else charactersOnDisplay += "2";
+                    }
                     break;
                 case "threeBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "3";
-                    else charactersOnDisplay += "3";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "3";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-3";
+                        else charactersOnDisplay += "3";
+                    }
                     break;
                 case "fourBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "4";
-                    else charactersOnDisplay += "4";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "4";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-4";
+                        else charactersOnDisplay += "4";
+                    }
                     break;
                 case "fiveBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "5";
-                    else charactersOnDisplay += "5";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "5";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-5";
+                        else charactersOnDisplay += "5";
+                    }
                     break;
                 case "sixBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "6";
-                    else charactersOnDisplay += "6";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "6";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-6";
+                        else charactersOnDisplay += "6";
+                    }
                     break;
                 case "sevenBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "7";
-                    else charactersOnDisplay += "7";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "7";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-7";
+                        else charactersOnDisplay += "7";
+                    }
                     break;
                 case "eightBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "8";
-                    else charactersOnDisplay += "8";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "8";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-8";
+                        else charactersOnDisplay += "8";
+                    }
                     break;
                 case "nineBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) charactersOnDisplay = "9";
-                    else charactersOnDisplay += "9";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) charactersOnDisplay = "9";
+                        else if (charactersOnDisplay.equals("-0")) charactersOnDisplay = "-9";
+                        else charactersOnDisplay += "9";
+                    }
                     break;
                 case "zeroBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (charactersOnDisplay.equals("0")) break;
-                    charactersOnDisplay += "0";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (charactersOnDisplay.equals("0")) break;
+                        else if (charactersOnDisplay.equals("-0")) break;
+                        charactersOnDisplay += "0";
+                    }
                     break;
                 case "dotBtn":
-                    if (charactersOnDisplay.length() >= maxSymbols) break;
-                    if (haveDot) break;
-                    haveDot = true;
-                    charactersOnDisplay += ",";
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() >= maxSymbols) break;
+                        if (haveDot) break;
+                        haveDot = true;
+                        charactersOnDisplay += ",";
+                    }
                     break;
                 case "acBtn":
                     charactersOnDisplay = "0";
                     actionDisplay.setText("");
+                    currentAction = 0;
                     haveDot = false;
+                    readOnly = false;
                     break;
-                case "idkBtn":
-                    if (charactersOnDisplay.length() > 1) {
-                        charactersOnDisplay = charactersOnDisplay.substring(0, charactersOnDisplay.length() - 1);
-                    }
-                    else if (charactersOnDisplay.length() == 1 && !charactersOnDisplay.equals("0")) {
+                case "cBtn":
+                    if (!readOnly) {
                         charactersOnDisplay = "0";
+                        haveDot = false;
+                    }
+                case "backspaceBtn":
+                    if (!readOnly) {
+                        if (charactersOnDisplay.length() > 1) {
+                            charactersOnDisplay = charactersOnDisplay.substring(0, charactersOnDisplay.length() - 1);
+                        } else if (charactersOnDisplay.length() == 1 && !charactersOnDisplay.equals("0")) {
+                            charactersOnDisplay = "0";
+                        }
                     }
                     break;
                 case "plusBtn":
                     animateTextChange(actionDisplay, "+");
                     currentAction = 1;
                     haveDot = false;
+                    readOnly = false;
                     firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
                     charactersOnDisplay = "0";
                     break;
@@ -128,6 +171,7 @@ public class MainController {
                     animateTextChange(actionDisplay, "-");
                     currentAction = 2;
                     haveDot = false;
+                    readOnly = false;
                     firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
                     charactersOnDisplay = "0";
                     break;
@@ -135,6 +179,7 @@ public class MainController {
                     animateTextChange(actionDisplay, "/");
                     currentAction = 3;
                     haveDot = false;
+                    readOnly = false;
                     firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
                     charactersOnDisplay = "0";
                     break;
@@ -142,27 +187,85 @@ public class MainController {
                     animateTextChange(actionDisplay, "*");
                     currentAction = 4;
                     haveDot = false;
+                    readOnly = false;
                     firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
                     charactersOnDisplay = "0";
+                    break;
+                case "percentBtn":
+                    animateTextChange(actionDisplay, "%");
+                    currentAction = 5;
+                    haveDot = false;
+                    readOnly = false;
+                    firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
+                    charactersOnDisplay = "0";
+                    break;
+                case "sqrtBtn":
+                    animateTextChange(actionDisplay, "r");
+                    currentAction = 5;
+                    readOnly = true;
+                    firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
+                    if (firstNumber < 0) {
+                        charactersOnDisplay = "UNDEFINED";
+                        break;
+                    }
+                    charactersOnDisplay = df.format(Math.sqrt(firstNumber)).replace(".", ",");
+                    break;
+                case "xinyBtn":
+                    animateTextChange(actionDisplay, "y");
+                    currentAction = 6;
+                    haveDot = false;
+                    readOnly = false;
+                    firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
+                    charactersOnDisplay = "0";
+                    break;
+                case "xinsquareBtn":
+                    animateTextChange(actionDisplay, "2");
+                    currentAction = 5;
+                    readOnly = true;
+                    firstNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
+                    charactersOnDisplay = df.format(Math.pow(firstNumber, 2)).replace(".", ",");
+                    break;
+                case "revertBtn":
+                    if (!readOnly) {
+                        charactersOnDisplay = "-" + charactersOnDisplay;
+                    }
                     break;
                 case "equalBtn":
                     animateTextChange(actionDisplay, "=");
                     switch (currentAction) {
                         case 1:
                             secondNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
-                            charactersOnDisplay = String.valueOf(firstNumber + secondNumber).replace(".", ",");
+                            charactersOnDisplay = df.format(firstNumber + secondNumber).replace(".", ",");
+                            readOnly = true;
                             break;
                         case 2:
                             secondNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
-                            charactersOnDisplay = String.valueOf(firstNumber - secondNumber).replace(".", ",");
+                            charactersOnDisplay = df.format(firstNumber - secondNumber).replace(".", ",");
+                            readOnly = true;
                             break;
                         case 3:
                             secondNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
-                            charactersOnDisplay = String.valueOf(firstNumber / secondNumber).replace(".", ",");
+                            readOnly = true;
+                            if (secondNumber == 0) {
+                                charactersOnDisplay = "UNDEFINED";
+                                break;
+                            }
+                            charactersOnDisplay = df.format(firstNumber / secondNumber).replace(".", ",");
                             break;
                         case 4:
                             secondNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
-                            charactersOnDisplay = String.valueOf(firstNumber * secondNumber).replace(".", ",");
+                            charactersOnDisplay = df.format(firstNumber * secondNumber).replace(".", ",");
+                            readOnly = true;
+                            break;
+                        case 5:
+                            secondNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
+                            charactersOnDisplay = df.format(firstNumber * secondNumber / 100).replace(".", ",");
+                            readOnly = true;
+                            break;
+                        case 6:
+                            secondNumber = Double.parseDouble(charactersOnDisplay.replace(",", "."));
+                            charactersOnDisplay = df.format(Math.pow(firstNumber, secondNumber)).replace(".", ",");
+                            readOnly = true;
                             break;
                         default:
                             break;
